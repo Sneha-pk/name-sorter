d8a581d7f4f72dc562ea6c22b622dbc9c5ac9682
SnehaPk <snehap.k0507@gmail.com>
2021-06-08 17:07:32 +1000

    Travis build

--- app.js
+++ app.js
@@ -3,14 +3,14 @@ const fs = require('fs')
 
 exports.sort_names = function() {
     let unsorted_names;
-    const content = fs.readFileSync('./unsorted-names-list.txt', 'utf8'); // Read the txt file from the O_DIRECTORY.
+    const content = fs.readFileSync('./unsorted-names-list.txt', 'utf8'); // Read the txt file from the directory.
     unsorted_names = content.split('\r\n');
     unsorted_names.sort((a, b) => {
         const x = a.split(' ');
         const y = b.split(' ');
         return x[x.length-1].toLowerCase() > y[y.length-1].toLowerCase() ? 1 : -1    // Compared in lower case to prevent errors due to case sensitivity.
     })
-    var stream = fs.createWriteStream("./sorted-names-list.txt");   //
+    var stream = fs.createWriteStream("./sorted-names-list.txt");   // create a stream to write the sorted list to the directory.
     stream.once('open', function(fd) {
     unsorted_names.forEach((item) => {
         stream.write(item + '\n');
@@ -18,5 +18,5 @@ exports.sort_names = function() {
     stream.end();
     });
 
-    return unsorted_names;
+    return unsorted_names; // return the sorted list of names to the screen.
 }
